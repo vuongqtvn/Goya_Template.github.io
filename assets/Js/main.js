4433721e3279033c50a54aad5496378511e02f86
DuongVuongUTE <quocvuongta1023@gmail.com>
2021-03-20 23:25:11 +0700

    first commit

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -32,3 +32,48 @@ console.log(mobile_close)
 mobile_close.addEventListener("click",()=> {
     slider.classList.remove("active")
 })
+let btn_cart = document.querySelector(".btn-shop-cart"); 
+let btn_cart_mb = document.querySelector(".btn-cart-mobile")
+let cart = document.querySelector(".cart-slider")
+let overlay = document.querySelector(".overlay")
+btn_cart.addEventListener("click",(event)=> {
+    event.preventDefault()
+    cart.classList.add("active")
+    overlay.style.display = "block"
+
+})
+btn_cart_mb.addEventListener("click",(event)=>{
+    event.preventDefault()
+    cart.classList.add("active")
+    overlay.style.display = "block"
+})
+let close_cart = document.querySelector(".close-cart") 
+
+close_cart.addEventListener("click",(event)=> {
+    event.preventDefault()
+    cart.classList.remove("active")
+    overlay.style.display = "none"
+
+})
+
+let mb_menu_2 = document.querySelector(".menu-mobile-2")
+let mb_menu_ic = document.querySelector(".header-mobile-menu")
+let close_mb_menu_2 = document.querySelector(".close-menu-mobile-2 button")
+mb_menu_ic.addEventListener("click",(event)=>{
+    event.preventDefault()
+    mb_menu_2.classList.add("active")
+    overlay.style.display = "block"
+})
+
+close_mb_menu_2.addEventListener("click",(event)=> {
+    event.preventDefault()
+    mb_menu_2.classList.remove("active")
+    overlay.style.display = "none"
+
+})
+overlay.addEventListener("click",(event)=> {
+    event.preventDefault()
+    cart.classList.remove("active")
+    mb_menu_2.classList.remove("active")
+    overlay.style.display = "none"
+})
